cfc61cb32f0a35dcc72a39d5795fc685b5911318
Daniel Petrov <dpetrov@appolica.com>
2021-06-18 15:40:12 +0300

    Add sample for using library in Fragment.

---
+++ InteractiveInfoWindowAndroid/sample/src/main/java/com/appolica/sample/activities/MapActivity.java
@@ -0,0 +1,18 @@
+package com.appolica.sample.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+
+import com.appolica.sample.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        Navigation.findNavController(findViewById(R.id.nav_host_fragment));
+    }
+}
---
+++ InteractiveInfoWindowAndroid/sample/src/main/java/com/appolica/sample/fragments/MapFragment.java
@@ -0,0 +1,108 @@
+package com.appolica.sample.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appolica.interactiveinfowindow.InfoWindow;
+import com.appolica.interactiveinfowindow.InfoWindowManager;
+import com.appolica.interactiveinfowindow.fragment.MapInfoWindowFragment;
+import com.appolica.sample.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment
+        extends Fragment
+        implements InfoWindowManager.WindowShowListener,
+        GoogleMap.OnMarkerClickListener {
+
+    private static final String RECYCLER_VIEW = "RECYCLER_VIEW_MARKER";
+    private static final String FORM_VIEW = "FORM_VIEW_MARKER";
+
+    private InfoWindow recyclerWindow;
+    private InfoWindow formWindow;
+    private InfoWindowManager infoWindowManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sample_map, container, false);
+
+        final MapInfoWindowFragment mapInfoWindowFragment =
+                (MapInfoWindowFragment) getChildFragmentManager().findFragmentById(R.id.infoWindowMapFragment);
+
+        infoWindowManager = mapInfoWindowFragment.infoWindowManager();
+        infoWindowManager.setHideOnFling(true);
+
+        mapInfoWindowFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                final Marker marker1 = googleMap.addMarker(new MarkerOptions().position(new LatLng(5, 5)).snippet(RECYCLER_VIEW));
+                final Marker marker2 = googleMap.addMarker(new MarkerOptions().position(new LatLng(1, 1)).snippet(FORM_VIEW));
+
+                final int offsetX = (int) getResources().getDimension(R.dimen.marker_offset_x);
+                final int offsetY = (int) getResources().getDimension(R.dimen.marker_offset_y);
+
+                final InfoWindow.MarkerSpecification markerSpec =
+                        new InfoWindow.MarkerSpecification(offsetX, offsetY);
+
+                recyclerWindow = new InfoWindow(marker1, markerSpec, new RecyclerViewFragment());
+                formWindow = new InfoWindow(marker2, markerSpec, new FormFragment());
+
+                googleMap.setOnMarkerClickListener(MapFragment.this);
+            }
+        });
+
+        infoWindowManager.setWindowShowListener(MapFragment.this);
+
+        return view;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        InfoWindow infoWindow = null;
+        switch (marker.getSnippet()) {
+            case RECYCLER_VIEW:
+                infoWindow = recyclerWindow;
+                break;
+            case FORM_VIEW:
+                infoWindow = formWindow;
+                break;
+        }
+
+        if (infoWindow != null) {
+            infoWindowManager.toggle(infoWindow, true);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onWindowShowStarted(@NonNull InfoWindow infoWindow) {
+//        Log.d("debug", "onWindowShowStarted: " + infoWindow);
+    }
+
+    @Override
+    public void onWindowShown(@NonNull InfoWindow infoWindow) {
+//        Log.d("debug", "onWindowShown: " + infoWindow);
+    }
+
+    @Override
+    public void onWindowHideStarted(@NonNull InfoWindow infoWindow) {
+//        Log.d("debug", "onWindowHideStarted: " + infoWindow);
+    }
+
+    @Override
+    public void onWindowHidden(@NonNull InfoWindow infoWindow) {
+//        Log.d("debug", "onWindowHidden: " + infoWindow);
+    }
+}
